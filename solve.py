6c537332ed61c8b5bdf629191b0179aa52b33c6c
Mithrosah <136345155+Mithrosah@users.noreply.github.com>
2026-01-01 12:26:08 +0800

    Initial commit

--- solve.py
+++ solve.py
@@ -32,8 +32,6 @@ with open('./instances.csv', 'r', encoding='utf-8') as f:
 # sampling configuration (instance-dependent steps)
 burn_in = 0
 sample_every = 1
-burn_in = 5000
-sample_every = 1
 seed = 42
 n_runs = 1000
 
