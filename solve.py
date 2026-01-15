c15b8398354c43857dc22d638b3d32c26b708f91
Mithrosah <136345155+Mithrosah@users.noreply.github.com>
2026-01-15 16:02:42 +0800

    Initial commit

--- solve.py
+++ solve.py
@@ -26,8 +26,8 @@ def parse_args():
     )
     p.add_argument(
         "--instances",
-        default="./instances.csv",
-        help="Path to instances.csv (default: ./instances.csv).",
+        default="./instances.jsonl",
+        help="Path to instances.jsonl (default: ./instances.jsonl).",
     )
     p.add_argument(
         "--max_workers",
