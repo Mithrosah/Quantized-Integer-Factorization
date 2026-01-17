22ea7aa793b865b9e929227e6b3d44a24f118aa5
Mithrosah <136345155+Mithrosah@users.noreply.github.com>
2026-01-17 16:28:33 +0800

    Initial commit

--- primes/generate_prime.py
+++ primes/generate_prime.py
@@ -2,7 +2,7 @@ from sympy import randprime, isprime
 
 
 def gen_semiprime(bits_N=512):
-    bits_p = bits_N // 2  # 256
+    bits_p = bits_N // 2
     lo = 1 << (bits_p - 1)
     hi = (1 << bits_p) - 1
 
@@ -19,7 +19,9 @@ def gen_semiprime(bits_N=512):
     N = p * q
     return p, q, N
 
-p, q, N = gen_semiprime(512)
-print("p bits:", p.bit_length(), "p:", p)
-print("q bits:", q.bit_length(), "q:", q)
-print("N bits:", N.bit_length(), "N:", N)
+for i in range(10):
+    p, q, N = gen_semiprime(64)
+    print("p bits:", p.bit_length(), "p:", p)
+    print("q bits:", q.bit_length(), "q:", q)
+    print("N bits:", N.bit_length(), "N:", N)
+    print()
